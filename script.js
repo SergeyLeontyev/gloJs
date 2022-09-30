928367d513d0090b562ace9958ddb6d1112241b7
SergeyLeontyev <46062910+SergeyLeontyev@users.noreply.github.com>
2022-09-30 13:41:29 +0700

    Второй урок

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var title;
-var screens;
-var screenPrice;
-var rollback;
-var fullPrice;
-var adaptive;
+var title = 'Новый проект';
+var screens = ["Простый","Сложные","Интерактивные"];
+var screenPrice = 1500;
+var rollback = 73;
+var fullPrice = 10000;
+var adaptive = true;
 
 alert('Тестовое сообщение');
 console.log('еще одно сообщени');
