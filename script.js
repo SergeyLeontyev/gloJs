aea97223fc2b385cbcb757420cce73f11050fd4e
SergeyLeontyev <46062910+SergeyLeontyev@users.noreply.github.com>
2022-09-30 13:53:07 +0700

    Выполненное задание

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 var title = 'Новый проект';
-var screens = ["Простый","Сложные","Интерактивные"];
+var screens = 'Простые, Сложные, Интерактивные';
 var screenPrice = 1500;
-var rollback = 73;
+var rollback = 13;
 var fullPrice = 10000;
 var adaptive = true;
 
-alert('Тестовое сообщение');
-console.log('еще одно сообщени');
+console.log(typeof(title));
+console.log(typeof(fullPrice));
+console.log(typeof(adaptive));
+
+console.log(screens.length);
+console.log('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей/долларов/гривен/юани');
+console.log('Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей/долларов/гривен/юани');
+
+console.log(screens.toLowerCase().split());
+
+console.log('Процент отката посреднику за работу' + ' ' + (fullPrice * (rollback / 100)));
