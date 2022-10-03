35f9ad71b67738dcfbb5e91c66b3938d158a45d5
SergeyLeontyev <46062910+SergeyLeontyev@users.noreply.github.com>
2022-10-04 02:18:10 +0700

    fix

--- hardPractice/script.js
+++ hardPractice/script.js
@@ -1,31 +1,26 @@
 let lang = prompt('Выберите язык', 'ru, en');
 let namePerson = prompt('Введите ваше имя');
+let language_arr = {en: "Mon, Tue, Wed, Thu, Fri, Sat, Sun", ru: "Пн,Вт,Ср,Чт,Пт,Сб,Вс"};
 
 if(lang == 'ru'){
-	console.log('Пн,Вт,Ср,Чт,Пт,Сб,Вс');
+	console.log(language_arr[lang]);
 } else if (lang == 'en'){
-	console.log('Mon, Tue, Wed, Thu, Fri, Sat, Sun');
+	console.log(language_arr[lang]);
 } else{
 	console.log('Вы неверно выбрали язык');
 }
 
 switch (lang){
 	case 'ru': 
-	console.log('Пн,Вт,Ср,Чт,Пт,Сб,Вс');
+	console.log(language_arr[lang]);
 	break
 	case 'en':
-		console.log('Mon, Tue, Wed, Thu, Fri, Sat, Sun');
+		console.log(language_arr[lang]);
 		break
 	default: 
 	console.log('Вы неверно выбрали язык');
 }
 
-
-
-
-let language_arr = [];
-language_arr['ru'] = ['Пн','Вт','СР','Чт','Пт','Сб','Вс'];
-language_arr['en'] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 console.log(language_arr[lang]);
 
 namePerson === "Артем" ?  console.log('Директор') : namePerson === "Александр" ? console.log('Преподаватель') : console.log('Студент');
