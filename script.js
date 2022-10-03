43cca8839115fba383148895b636c7587eacb241
SergeyLeontyev <46062910+SergeyLeontyev@users.noreply.github.com>
2022-10-03 20:20:29 +0700

    Задание 3

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-let title = 'Новый проект';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 1500;
+'use strict';
+
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+let screenPrice = parseInt(prompt("Сколько будет стоить данная работа?", "12000"));
+let adaptive = confirm("Нужен ли адаптив?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = parseInt(prompt("Сколько это будет стоить?"));
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = parseInt(prompt("Сколько это будет стоить?"));
 let rollback = 13;
-let fullPrice = 10000;
-let adaptive = true;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
 
-console.log(typeof(title));
-console.log(typeof(fullPrice));
-console.log(typeof(adaptive));
+console.log(title);
+console.log(screens);
+console.log(screenPrice);
+console.log(adaptive);
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
+console.log(rollback);
+console.log(`Общая сумма составляет ${fullPrice} рублей.`);
+console.log(`После вычета отката посреднику ${servicePercentPrice}`);
 
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
 
-console.log(screens.toLowerCase().split(", "));
-
-console.log(`Процент отката посреднику за работу ${(fullPrice * (rollback / 100))}`);
+if(fullPrice >= 30000 ){
+	servicePercentPrice = screenPrice - (servicePercentPrice *= 0.1);
+	console.log(`Стоимость услуги с учетом скидки в 10% составляет ${servicePercentPrice} рублей.`);
+} else if(fullPrice < 30000 || fullPrice >= 15000){
+	servicePercentPrice = screenPrice - (servicePercentPrice *= 0.05);
+	console.log(`Стоимость услуги с учетом скидки в 5% составляет ${servicePercentPrice} рублей.`);
+} else if(fullPrice < 15000 || fullPrice >= 0){
+	console.log('Скидка не предусмотрена');
+} else {
+	console.log('Что то пошло не так');
+}
 
 
