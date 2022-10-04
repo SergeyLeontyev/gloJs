ebbb4668ecc1842854d3db210a1e3bbe005076e1
SergeyLeontyev <46062910+SergeyLeontyev@users.noreply.github.com>
2022-10-05 01:05:03 +0700

    lesson04

--- hardPractice/script.js
+++ hardPractice/script.js
@@ -1,26 +1,17 @@
-let lang = prompt('Выберите язык', 'ru, en');
-let namePerson = prompt('Введите ваше имя');
-let language_arr = {en: "Mon, Tue, Wed, Thu, Fri, Sat, Sun", ru: "Пн,Вт,Ср,Чт,Пт,Сб,Вс"};
+const string = function (arg) {
+	let res = 'Введите текст, а не число';
+	if (typeof arg === "string") {
+					const maxLength = 30;
+					const str = arg.trim();
+					if (str.length > maxLength) {
+									res = str.substr(0,maxLength) + '...';
+					} else {
+									res = str;
+					}
+	}
+	return res;
+};
 
-if(lang == 'ru'){
-	console.log(language_arr[lang]);
-} else if (lang == 'en'){
-	console.log(language_arr[lang]);
-} else{
-	console.log('Вы неверно выбрали язык');
-}
-
-switch (lang){
-	case 'ru': 
-	console.log(language_arr[lang]);
-	break
-	case 'en':
-		console.log(language_arr[lang]);
-		break
-	default: 
-	console.log('Вы неверно выбрали язык');
-}
-
-console.log(language_arr[lang]);
-
-namePerson === "Артем" ?  console.log('Директор') : namePerson === "Александр" ? console.log('Преподаватель') : console.log('Студент');
+console.log('string(): ', string(3));
+console.log('string(): ', string('    Строка менее 30 символов     '));
+console.log('string(): ', string('    Строка более 30 символов - ну очень при очень длинная строчка получилась!'));
--- script.js
+++ script.js
@@ -8,29 +8,61 @@ const service1 = prompt("Какой дополнительный тип услу
 const servicePrice1 = parseInt(prompt("Сколько это будет стоить?"));
 const service2 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice2 = parseInt(prompt("Сколько это будет стоить?"));
-const rollback = 13;
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+const rollback = 13 / 100;
 
-console.log(title);
-console.log(screens);
-console.log(adaptive);
-console.log(`Общая сумма составляет ${fullPrice} рублей.`);
-console.log(`Общая сумма составляет ${fullPrice} рублей.`);
-console.log(`После вычета отката посреднику ${servicePercentPrice}`);
+const getAllServicePrices = function () {
+	return servicePrice1 + servicePrice2;
+};
+const allServicePrices = getAllServicePrices();
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
+
+const servicePercentPrice = getServicePercentPrices();
 
 
-if(fullPrice >= 30000 ){
-	servicePercentPrice = Math.ceil(screenPrice - (servicePercentPrice *= 0.1));
-	console.log(`Стоимость услуги с учетом скидки в 10% составляет ${servicePercentPrice} рублей.`);
-} else if(fullPrice < 30000 || fullPrice >= 15000){
-	servicePercentPrice = Math.ceil(screenPrice - (servicePercentPrice *= 0.05));
-	console.log(`Стоимость услуги с учетом скидки в 5% составляет ${servicePercentPrice} рублей.`);
-} else if(fullPrice < 15000 || fullPrice >= 0){
-	console.log('Скидка не предусмотрена');
-} else {
-	console.log('Что то пошло не так');
+function getFullPrice (int1,int2){
+	return int1 + int2;
 }
+
+function getTitle (title){
+	let titleNew = title.trim();
+	return titleNew[0].toUpperCase() + titleNew.slice(1).toLowerCase();
+}
+
+function getServicePercentPrices (){
+	return Math.ceil(fullPrice - (fullPrice * rollback));
+}
+
+
+const showTypeOf = function (variable) {
+	console.log(variable, typeof variable);
+};
+
+
+const getRollbakMessage = function (price) {
+	if (price >= 30000) {
+		return "Даём скидку в 10%";
+	} else if (price < 30000 || price >= 15000) {
+		return "Даём скидку в 5%";
+	} else if (price < 15000 || price >= 0) {
+		return "Скидка не предусмотрена";
+	} else {
+		return "Что то пошло не так";
+	}
+};
+
+
+console.log(`Название сайта ${getTitle(title)}`);
+showTypeOf(title);
+showTypeOf(screens);
+showTypeOf(adaptive);
+
+console.log(getRollbakMessage(fullPrice));
+console.log(`Общая сумма составляет ${fullPrice} рублей.`);
+console.log(`Общая сумма составляет ${fullPrice} рублей.`);
+console.log(`После вычета отката посреднику ${getServicePercentPrices()}`);
+
+
+
 
 
 
