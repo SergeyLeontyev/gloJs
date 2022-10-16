f5a184b256949c570319153e15b3a958e8ddc139
SergeyLeontyev <46062910+SergeyLeontyev@users.noreply.github.com>
2022-10-17 01:02:43 +0700

    lesson11

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
+// 1) Повесить на кнопку обработчик события click и реализовать такой функционал:
+// 			В input[type=text] можно ввести цвет: red, green, blue и так далее.
+// 			По нажатию на кнопку необходимо брать этот цвет и добавлять его свойству style="backgroundColor: " квадрата
+// 			Работать должно так: ввели в input[type=text] yellow, по нажатию на кнопку значение input[type=text] попадает в свойство style="backgroundColor: yellow" и фон квадрата должен поменяться
+// 2) В кружке (который внутри квадрата) есть кнопка. Дать ей свойство style="display: none; " 
+// 3) Повесить на input[type=range] обработчик события input и реализовать такой функционал:
+// при каждом изменении положения ползунка значение input[type=range] необходимо заносить в свойства ширины и высоты кружка (который внутри квадрата) (height и width) (в процентах!!) 
+
+let btn = document.getElementById('btn');
+let inputText = document.getElementById('text');
+let square = document.getElementById('square');
+let inputRange = document.getElementById('range');
+let eBtn = document.getElementById('e_btn');
 
 
+const func1 = function () {
+	console.log(inputText.value);
+	square.style.background = inputText.value;
+}
+
+const func2 = function (e) {
+	square.style.height = e.target.value + '%';
+	square.style.width = e.target.value + '%';
+}
+
+btn.addEventListener('click', func1);
+
+document.addEventListener('DOMContentLoaded', () => {
+	eBtn.style.display = "none";
+})
+
+inputRange.addEventListener('input', func2);
+
+
+
+
+console.log(btn);
+console.log(inputText);
+console.log(square);
+console.log(inputRange);
 
 
 
